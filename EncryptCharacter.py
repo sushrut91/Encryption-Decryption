b66570f370b7d925f78e69ea9cb295bd94b8029f
Sushrut <sushrut91@gmail.com>
2016-08-29 15:36:49 -0400

    Implementation of Decryption

--- EncryptCharacter.py
+++ EncryptCharacter.py
@@ -1,45 +1,82 @@
 #!/usr/bin/python3
 
 def main():
-    letters = {'a':0, 'b':1, 'c':2 , 'd':3 , 'e':4, 'f':5 , 'g':6, 'h':7, 'i':8}
-    input_char = 'i'
+    #letters = {'a':0, 'b':1, 'c':2 , 'd':3 , 'e':4, 'f':5 , 'g':6, 'h':7, 'i':8}
+    letters = {'a':0, 'b':1, 'c':2 , 'd':3 , 'e':4, 'f':5 , 'g':6}
+    input_char = 'g'
+    # b denotes the shift 
+    a ,b = 2,3
     
     if input_char in letters:
-        cipher_char = EncryptCharacter(input_char,letters)
+        cipher_char = EncryptCharacter(input_char,letters,a,b)
         print(cipher_char)
+        plain_char = DecryptCharacter(cipher_char, letters,a,b)
+        print(plain_char)
     else:
         print("Please input correct letters")
 
-def EncryptCharacter(plain_char, letter_set):
+def DecryptCharacter(encrypt_char, letter_set, a, b):
+    #P = a'(x)-b mod p
+    #a' = a^(p-2) mod n
+    n = len(letter_set)
+    plain_char = encrypt_char
+    
+    if AreRelativePrime(a, n):
+        a_inverse = (a ** (n-2)) % n
+        plain_char = ((a_inverse * encrypt_char) + (-a_inverse * b)) % n
+        print(plain_char)
+        for key,value in letter_set.items():
+            if value == plain_char:
+                return key
+    else:
+        return "Please change value of a such that a & n are coprime numbers, n is a prime number, n>a"
+    
+def EncryptCharacter(plain_char, letter_set,a,b):
     # E(x) = (a(x) + b) mod p where a and p should be co-prime
     x = letter_set.get(plain_char)
-    print(x)
-    a = 2
     n = len(letter_set)
-    b = 3
     cipher_char = plain_char
     
     if AreRelativePrime(a,n):
         cipher_char = (x + b) % n
         return cipher_char
     else:
-        return 'Please change value of a such that a & n are coprime numbers'
+        return 'Please change value of a such that a & n are coprime numbers, n is a prime number, n>a'
         
 def AreRelativePrime(n1,n2):
 # Numbers are relative prime if their GCD is 1, Euclid's algorithm
 # Ensure n1 is always greater
-    if n2 > n1 :
-        swap = n2
-        n2 = n1
-        n1 = swap
-    remainder = n1
-    while n2 > 1:
-        remainder = n1 % n2
-        n1 = n2
-        n2 = remainder
-    if remainder == 1:
-        return True
+# Ensure that n1 is always prime
+    flag = False
+    
+    if n1 > n2:
+        flag = isPrime(n1)
+    elif n2> n1:
+        flag = isPrime(n2)
     else:
-        return False
+        return 'Both a and n cannot be equal for algorithm to work'
+        
+    if flag:
+        if n2 > n1 :
+            swap = n2
+            n2 = n1
+            n1 = swap
+        remainder = n1
+        while n2 > 1:
+            remainder = n1 % n2
+            n1 = n2
+            n2 = remainder
+        if remainder == 1:
+            return True
+        else:
+            return False
+    else:
+        return False    
+def isPrime(n):
+    flag = True
+    for i in range(2,n):
+        if n % i == 0:
+            flag = False
+    return flag    
 #Run the function main() at startup of the script    
 if __name__ == "__main__": main()
